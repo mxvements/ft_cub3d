b50696f4486332e42df1d81d7070e8edb6a9172a
luciama2 <luciama2@student.42madrid.com>
2025-01-14 20:53:38 +0100

    added perspective

--- src/game/put_line.c
+++ src/game/put_line.c
@@ -23,49 +23,55 @@ static float	distance(float row2, float row1, float col2, float col1, t_cub *cub
 	float	delta_col;
 	float	distance;
 
-	delta_row = row2 - row1;
-	delta_col = col2 - col1;
+	delta_row = (row1 - row2);
+	delta_col = (col1 - col2);
 	distance = sqrt(delta_row * delta_row + delta_col * delta_col);
+	// printf("Player Position: (%f, %f), Ray Hit: (%f, %f), Distance: %f\n", row1, col1, row2, col2, distance);
 	//this distances has a fisheye effect this fix
 	float angle = atan2(delta_col, delta_row)- cub->map->player->angle; //POSIBLE BUG
-	float fix_distance = distance * cos(angle);
+	float fix_distance = abs(distance * cos(angle));
+	printf("dist: %f, fixed: %f\n", distance, fix_distance);
 	return (fix_distance);
 }
 void	put_line(t_player *player, t_cub *cub, float angle, int i, int color)
 {
 	float	cos_angle;
 	float	sin_angle;
+	const float player_row = player->x * MINIMAP_TILE_SIZE + cub->minimap->start_x
+		+ MINIMAP_TILE_SIZE / 2;
+	const float player_col  = player->y * MINIMAP_TILE_SIZE + MINIMAP_TILE_SIZE / 2;
 	float	ray_row;
 	float	ray_col;
 
 	float	dist;
 	float	wall_height;
 	int		start_row;
-	int		end;
+	int		end_row;
 
 	cos_angle = cos(angle);
 	sin_angle = sin(angle);
-	ray_row = player->x * MINIMAP_TILE_SIZE + cub->minimap->start_x
-		+ MINIMAP_TILE_SIZE / 2;
-	ray_col = player->y * MINIMAP_TILE_SIZE + MINIMAP_TILE_SIZE / 2;
+	ray_row = (float)player_row;
+	ray_col = (float)player_col;
 	// FOV on minimap
 	while (!touch_wall(ray_col, ray_row, cub))
 	{
-		
 		put_pixel((int)ray_col, (int)ray_row, color, cub);
 		ray_row += cos_angle;
 		ray_col += sin_angle;
 	}
 	//perspective
-	dist = distance(ray_row, player->x, ray_col, player->y, cub);
-	wall_height = (32 / dist) * (WIN_WIDTH / 2);
-	start_row = (WIN_HEIGHT - wall_height) / 2;
-	end = start_row + wall_height;
-	while (start_row < end)
+	dist = distance(ray_row, player_row, ray_col, player_col, cub);
+	wall_height = (WIN_HEIGHT * 10) / dist; // * (WIN_WIDTH / 2);  //WALL_SIZE
+	// start_row = (WIN_HEIGHT - wall_height) / 2;
+	// end_row = start_row + wall_height;
+	// printf("dist, start: %f\n", dist);
+	start_row = ((WIN_HEIGHT - wall_height) / 2);
+	end_row = (start_row + wall_height);
+	while (end_row > start_row)
 	{
 		//hay que controlar que no dibuje encima del minimapa
-		put_pixel(i, start_row, 255, cub);
-		start_row++;
+		put_pixel(i, end_row, 255, cub);
+		end_row--;
 	}
 }
 
@@ -85,7 +91,6 @@ void	minimap_put_axis(t_cub *cub, int color)
 	sin_angle = sin(player->angle);
 	while (!touch_wall(ray_col, ray_row, cub))
 	{
-
 		put_pixel((int)ray_col, (int)ray_row, color, cub);
 		ray_row += cos_angle;
 		ray_col += sin_angle;
@@ -97,16 +102,16 @@ void	put_camera(t_cub *cub)
 	const float	FOV = PI / 6;
 	float		fraction;
 	float		start_angle;
-	int			i;
+	int			screen_col_idx;
 
 	player = cub->map->player;
-	fraction = (FOV * 2) / (cub->map->cols * MINIMAP_TILE_SIZE);
+	fraction = (FOV * 2) / WIN_WIDTH; //(FOV * 2) / (cub->map->cols * MINIMAP_TILE_SIZE)
 	start_angle = player->angle - FOV;
-	i = 0;
-	while (i < WIN_WIDTH) //i < (cub->map->cols * MINIMAP_TILE_SIZE)
+	screen_col_idx = WIN_WIDTH;
+	while (screen_col_idx > 0) //i < (cub->map->cols * MINIMAP_TILE_SIZE)
 	{
-		put_line(player, cub, start_angle, i, 0xAA6666);
+		put_line(player, cub, start_angle, screen_col_idx, 0xAA6666);
 		start_angle += fraction;
-		i++;
+		screen_col_idx--;
 	}
 }
