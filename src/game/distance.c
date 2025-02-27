7e08703e950fb7650014f0696e4503e6c5295d83
luciama2 <luciama2@student.42madrid.com>
2025-02-27 18:38:30 +0100

    added step on collisions calculation

--- src/game/distance.c
+++ src/game/distance.c
@@ -7,6 +7,8 @@ static float	fix_fisheye(t_cub *cub, float delta_row, float delta_col,
 	float	fixed_dist;
 
 	// this distances has a fisheye effect
+	if (d < 1.0f)
+		return 0.99f;
 	angle = atan2(delta_col, delta_row) - cub->map->player->angle;
 	// POSIBLE BUG on a atan2
 	fixed_dist = fabs(d * cos(angle));
@@ -55,6 +57,7 @@ void	get_distance(t_cub *cub, float cos_f, float sin_f, int i)
 	const t_player	*pl = cub->map->player;
 	float prev_ray[2]; //[0] == row, [1] == col
 	float ray[2];      //[0] == row, [1] == col
+	const float	step = 0.02f;
 
 	ray[0] = (float)pl->win_row;
 	ray[1] = (float)pl->win_col;
@@ -62,8 +65,8 @@ void	get_distance(t_cub *cub, float cos_f, float sin_f, int i)
 	{
 		prev_ray[0] = ray[0];
 		prev_ray[1] = ray[1];
-		ray[0] += cos_f;
-		ray[1] += sin_f;
+		ray[0] += cos_f * step;
+		ray[1] += sin_f * step;
 	}
 	cub->hits[i].ray[0] = ray[0];
 	cub->hits[i].ray[1] = ray[1];
